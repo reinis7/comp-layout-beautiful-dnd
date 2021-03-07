ca46e76a957532d61492e92b8e52544e8d7d6295
reinis7 <lat.lag2012@gmail.com>
2021-03-07 22:00:18 +0000

    first commit

--- src/store/actions/layout.js
+++ src/store/actions/layout.js
@@ -38,6 +38,7 @@ export const chooseComponent = (item) => async dispatch => {
   dispatch({ type: types.CHOOSE_COMPONENT_ITEM, payload: item });
 };
 
+//  code submit
 export const submitCodes = (codelines) => async dispatch => {
   dispatch({ type: types.SUBMIT_CODE_LINES, payload: codelines });
 };
